385ea6ee8cb23b818ee3c6128b2a5979a955b4c8
uTkarsHH001 <utk.arshh.9.2@gmail.com>
2024-12-21 11:32:45 +0530

    Added Files

---
+++ src/Components/SideBar.tsx
@@ -0,0 +1,125 @@
+import {useEffect, useRef, useState} from "react";
+import { FaBook, FaCloud, FaCode, FaPhoneAlt } from "react-icons/fa";
+import { IoIosArrowDown, IoMdSettings } from "react-icons/io";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdOutlineExitToApp } from "react-icons/md";
+import { RxCross2, RxHamburgerMenu } from "react-icons/rx";
+
+const SideBar = () => {
+    const [isSideBarVisible, setIsSideBarVisible] = useState(false)
+    const [isMobile, setIsMobile] = useState(false)
+    const isMobileRef = useRef(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth
+            const isMobileLocal = width < 768
+            if(isMobileLocal){
+                setIsMobile(true)
+                isMobileRef.current = true
+            }else{
+                setIsMobile(false)
+                isMobileRef.current = false
+            }
+        }
+
+        handleResize()
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, []);
+
+    return (
+        <div className="flex flex-col w-full md:w-64 bg-white md:h-screen border-r relative z-20">
+            <div className={`flex justify-between md:justify-around w-full px-4 items-center`}>
+                <div className="flex items-center justify-center gap-4 h-16">
+                    <img src={'/svg/logo.svg'} alt={'logo'} className={`w-6`}/>
+                    <h1 className="text-lg font-semibold">CodeAnt AI</h1>
+                </div>
+
+                <div className={`flex md:hidden`}>
+                   <button className={``} onClick={()=>{setIsSideBarVisible(prevState => !prevState)}}>
+                        {isSideBarVisible ? <RxCross2 /> : <RxHamburgerMenu />} 
+                   </button>
+                </div>
+            </div>
+
+            <div
+                className={`absolute md:static bottom-0 w-full h-screen md:h-full bg-black/10 translate-y-full md:translate-y-0 flex overflow-hidden transition-all `}
+                style={{
+                    height: isMobile ? (isSideBarVisible ? 'calc(100vh - 64px)' : '0px') : '100%',
+                }}
+            >
+                <div
+                    className={`bg-white w-full h-fit md:h-full flex flex-col`}>
+                    <div className="flex flex-col items-center py-4">
+                <span className="mt-2 w-11/12 text-sm border p-2 rounded-xl flex gap-2 items-center justify-between ">
+                    <span>UtkarshDhairyaPanwar</span>
+                    <IoIosArrowDown />
+                </span>
+                    </div>
+
+                    <nav className="flex-1 px-4 font-medium py-2 space-y-2">
+                        <a
+                            href="#"
+                            className="flex items-center px-4 py-2 text-sm bg-[#1570EF] text-white rounded-lg"
+                        >
+                            <IoHomeOutline className="text-lg" />
+                            <span className="ml-3">Repositories</span>
+                        </a>
+                        <a
+                            href="#"
+                            className="flex items-center px-4 py-2 text-sm text-gray-700 rounded-lg hover:bg-gray-100"
+                        >
+                            <FaCode className="text-lg"/>
+                            <span className="ml-3">AI Code Review</span>
+                        </a>
+                        <a
+                            href="#"
+                            className="flex items-center px-4 py-2 text-sm text-gray-700 rounded-lg hover:bg-gray-100"
+                        >
+                            <FaCloud className="text-lg"/>
+                            <span className="ml-3">Cloud Security</span>
+                        </a>
+                        <a
+                            href="#"
+                            className="flex items-center px-4 py-2 text-sm text-gray-700 rounded-lg hover:bg-gray-100"
+                        >
+                            <FaBook className="text-lg"/>
+                            <span className="ml-3">How to Use</span>
+                        </a>
+                        <a
+                            href="#"
+                            className="flex items-center px-4 py-2 text-sm text-gray-700 rounded-lg hover:bg-gray-100"
+                        >
+                            <IoMdSettings className="text-lg"/>
+                            <span className="ml-3">Settings</span>
+                        </a>
+                    </nav>
+
+                    <div className="px-4 py-4">
+                        <a
+                            href="#"
+                            className="flex items-center px-4 py-2 text-sm font-medium text-gray-700 rounded-lg hover:bg-gray-100"
+                        >
+                            <FaPhoneAlt className="text-lg"/>
+                            <span className="ml-3 font-light">Support</span>
+                        </a>
+                        <a
+                            href="#"
+                            className="flex items-center px-4 py-2 text-sm font-medium text-gray-700 rounded-lg hover:bg-gray-100"
+                        >
+                            <MdOutlineExitToApp className="text-lg" />
+                            <span className="ml-3">Logout</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SideBar;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,86 @@
+import { MdOutlineRefresh } from "react-icons/md";
+import Layout from "../Components/Layout.tsx";
+import {DataMock} from "../data.ts";
+import {ChangeEvent, useState} from "react";
+import { CiCirclePlus, CiSearch } from "react-icons/ci";
+import { GrStorage } from "react-icons/gr";
+
+const Home = () => {
+    const [filterdData, setFilterdData] = useState(DataMock)
+
+    const handleFilter = (e : ChangeEvent<HTMLInputElement>) =>{
+        const value = e.target.value
+        const filtered = DataMock.filter(item =>
+            item.title.toLowerCase().includes(value.toLowerCase()) ||
+            item.language.toLowerCase().includes(value.toLowerCase())
+        )
+        setFilterdData(filtered)
+    }
+    return (
+        <Layout>
+            <div className={`p-2`}>
+                <div className={`bg-white w-full rounded-xl border flex flex-col`}>
+                    <div className={`flex flex-col gap-4 border-b p-4`}>
+                        <div className={`flex justify-between items-center flex-wrap gap-4`}>
+                            <div className={`flex flex-col`}>
+                                <span className={`font-semibold text-xl`}>Repositories</span>
+                                <span className={`font-light text-sm`}>33 total repositories</span>
+                            </div>
+                            <div className={`flex gap-2`}>
+                                <button className={`flex text-xs items-center p-2 px-4 gap-2 rounded-md border`}>
+                                    <MdOutlineRefresh className="text-lg" />
+                                    Refresh All
+                                </button>
+                                <button
+                                    className={`flex text-xs items-center p-2 px-4 gap-2 bg-[#1570EF] text-white rounded-md`}>
+                                        <CiCirclePlus className="text-lg" />
+                                        Add Repository
+                                </button>
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor={`inputSearch`}
+                                   className={`border w-full md:w-fit rounded-md flex gap-2 items-center px-2 py-2`}>
+                                    <CiSearch />
+                                    <input id={`inputSearch`} placeholder={`Search Repositories`}
+                                       onChange={handleFilter}
+                                       className={`text-xs w-[200px] placeholder:text-gray-700 outline-none`}
+                                    />
+                            </label>
+                        </div>
+                    </div>
+                    <div className={`flex flex-col w-full`}>
+                        {
+                            filterdData.length >= 1 ? filterdData.map((item, index) => (
+                                <div key={index} className={`flex flex-col p-4 gap-3 hover:bg-gray-100 ${index !== filterdData.length-1 ? 'border-b':''}`}>
+                                    <div className={`flex gap-2 items-center`}>
+                                        <span>{item.title}</span>
+                                        <span
+                                            className={`bg-[#EFF8FF] border border-[#B2DDFF] text-primary text-xs px-2 rounded-full`}>{item.type}</span>
+                                    </div>
+                                    <div className={`flex gap-4 md:gap-8 text-sm font-light`}>
+                                        <span className={`flex items-center gap-2`}>
+                                            {item.language}
+                                            <span className={`bg-primary opacity-75 p-1 rounded-full`}></span>
+                                        </span>
+                                        <span className={`flex items-center gap-2`}>
+                                            <GrStorage />
+                                            {item.size}
+                                        </span>
+                                        <span>{item.updatedAt}</span>
+                                    </div>
+                                </div>
+                            )): (
+                                <div className={`flex flex-col border-b p-4 gap-3 text-gray-500 text-center`}>
+                                    Looks like there is no repository to show.
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default Home;
